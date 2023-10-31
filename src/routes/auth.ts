fc07820e95d813aec1727862cacf19b39e60aaf1
Ahmed Osama Blue <ahmed.osama1982002@gmail.com>
2023-10-31 18:38:18 +0200

    chore: API Documentation for Notifications and Authentication
    Added the API documentation for the following:
    
    - Login
    - SignUp
    - ForgetPassword
    - Google OAuth
    - Verify Email
    - Get Notifications by user

---
+++ src/routes/auth.ts
@@ -0,0 +1,185 @@
+import express from 'express';
+const router = express.Router();
+
+/**
+ * @openapi
+ * '/api/v1/auth/google':
+ *  get:
+ *     tags:
+ *     - Authentication
+ *     summary: Register a user using Google (OAuth)
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *     responses:
+ *       "200":
+ *         description: Success
+ *         content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LoginResponse'
+ *       "400":
+ *        $ref: '#/responses/400'
+ *       "401":
+ *        $ref: '#/responses/401'
+ *       "404":
+ *        $ref: '#/responses/404'
+ *       "403":
+ *        $ref: '#/responses/403'
+ *       "408":
+ *        $ref: '#/responses/408'
+ *       "409":
+ *        $ref: '#/responses/409'
+ *       "410":
+ *        $ref: '#/responses/410'
+ */
+router.route('/google').get();
+
+/**
+ * @openapi
+ * '/api/v1/auth/signup':
+ *  post:
+ *     tags:
+ *     - Authentication
+ *     summary: Register a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/CreateUserInput'
+ *     responses:
+ *       "200":
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LoginResponse'
+ *       "400":
+ *        $ref: '#/responses/400'
+ *       "401":
+ *        $ref: '#/responses/401'
+ *       "404":
+ *        $ref: '#/responses/404'
+ *       "403":
+ *        $ref: '#/responses/403'
+ *       "408":
+ *        $ref: '#/responses/408'
+ *       "409":
+ *        $ref: '#/responses/409'
+ *       "410":
+ *        $ref: '#/responses/410'
+ */
+router.route('/signup').post();
+
+/**
+ * @openapi
+ * '/api/v1/auth/login':
+ *  post:
+ *     tags:
+ *     - Authentication
+ *     summary: Login a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/LoginRequest'
+ *     responses:
+ *       "200":
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LoginResponse'
+ *       "400":
+ *        $ref: '#/responses/400'
+ *       "401":
+ *        $ref: '#/responses/401'
+ *       "404":
+ *        $ref: '#/responses/404'
+ *       "403":
+ *        $ref: '#/responses/403'
+ *       "408":
+ *        $ref: '#/responses/408'
+ *       "409":
+ *        $ref: '#/responses/409'
+ *       "410":
+ *        $ref: '#/responses/410'
+ *
+ *
+ */
+router.route('/login').post();
+
+/**
+ * @openapi
+ * '/api/v1/auth/forget-password':
+ *  post:
+ *     tags:
+ *     - Authentication
+ *     summary: Login a user
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/ForgotPasswordRequest'
+ *     responses:
+ *       "200":
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/LoginResponse'
+ *       "400":
+ *        $ref: '#/responses/400'
+ *       "401":
+ *        $ref: '#/responses/401'
+ *       "404":
+ *        $ref: '#/responses/404'
+ *       "403":
+ *        $ref: '#/responses/403'
+ *       "408":
+ *        $ref: '#/responses/408'
+ *       "409":
+ *        $ref: '#/responses/409'
+ *       "410":
+ *        $ref: '#/responses/410'
+ *
+ *
+ */
+router.route('/forget-password').post();
+
+/**
+ * @openapi
+ * '/api/v1/auth/verify-email/{token}':
+ *  get:
+ *     tags:
+ *     - Authentication
+ *     summary: Verify your email by clicking on the provided link
+ *     responses:
+ *       "200":
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/VerifyEmailResponse'
+ *       "400":
+ *        $ref: '#/responses/400'
+ *       "401":
+ *        $ref: '#/responses/401'
+ *       "404":
+ *        $ref: '#/responses/404'
+ *       "403":
+ *        $ref: '#/responses/403'
+ *       "408":
+ *        $ref: '#/responses/408'
+ *       "409":
+ *        $ref: '#/responses/409'
+ *       "410":
+ *        $ref: '#/responses/410'
+ *
+ *
+ */
+router.route('/verify-email/:token').get();
