d14bbaa07073c9de3833d5f6cdcd2de4287f5e01
Ahmed Zahran <87142232+AhmedZahran02@users.noreply.github.com>
2023-11-28 09:57:21 +0200

    Revert "feat: GET tweets like username"
    This reverts commit 1491b69f21fbc0cc96e8b2f0b49204c3dde2a71b.

--- src/repositories/tweetRepository.ts
+++ src/repositories/tweetRepository.ts
@@ -1,20 +1,87 @@
 import prisma from '../client';
+import { getTweetEntities } from './entityRepository';
 
+export const getTweetAndUserById = async (tweetId: string) => {
+  const tweet = await prisma.tweet.findUnique({
+    where: {
+      id: tweetId,
+    },
+    include: {
+      TweetEntity: true,
+    },
+  });
+  const user = await prisma.user.findUnique({
+    where: {
+      id: tweet?.userId,
+    },
+  });
+  return { tweet: tweet, tweetingUser: user };
+};
 
-
-export const getTweetAndUserById=async (tweetId: string) => {
-    const tweet= await prisma.tweet.findUnique({
-      where: {
-        id: tweetId,
+export const getTweetsLikedById = async (
+  Id: string,
+  skip: number,
+  parsedLimit: number,
+) => {
+  const tweets = await prisma.like.findMany({
+    where: {
+      userId: Id,
+      liked: {
+        deletedAt: null,
       },
-      include: {
-        TweetEntity: true,
+    },
+    select: {
+      liked: {
+        select: {
+          createdAt: true,
+          id: true,
+          text: true,
+          source: true,
+          coordinates: true,
+          author: {
+            select: {
+              name: true,
+              birthDate: true,
+              location: true,
+              url: true,
+              description: true,
+              verified: true,
+              followersCount: true,
+              followingCount: true,
+              createdAt: true,
+              profileBannerUrl: true,
+              profileImageUrl: true,
+              email: true,
+              userName: true,
+            },
+          },
+          replyToTweetId: true,
+          replyCount: true,
+          retweetedId: true,
+          retweetCount: true,
+          qouteTweetedId: true,
+          qouteCount: true,
+          likesCount: true,
+          sensitive: true,
+        },
       },
-    });
-    const user= await prisma.user.findUnique({
-        where:{
-            id:tweet?.userId
-        }
-    });
-    return {tweet:tweet,tweetingUser:user}
-  };
+    },
+    orderBy: {
+      liked: {
+        createdAt: 'desc',
+      },
+    },
+    skip,
+    take: parsedLimit,
+  });
+
+  const likedTweets = [];
+
+  for (const tweet of tweets) {
+    const entities = await getTweetEntities(tweet.liked.id);
+    const temp = { ...tweet.liked, entities: entities };
+    likedTweets.push(temp);
+  }
+
+  return likedTweets;
+};
--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -8,56 +8,59 @@ export const getUserByUsername = async (user_name: string) => {
   });
 };
 
-
-export const getUserBlocked=async (blockingUser: string,blockedUser: string) => {
+export const getUserBlocked = async (
+  blockingUser: string,
+  blockedUser: string,
+) => {
   return await prisma.block.findUnique({
     where: {
-      blockerId_blockedId:{blockedId:blockedUser,blockerId:blockingUser}
+      blockerId_blockedId: { blockedId: blockedUser, blockerId: blockingUser },
     },
   });
 };
 
-
-export const blockUserByIDs=async (blockingUser: string,blockedUser: string) => {
+export const blockUserByIDs = async (
+  blockingUser: string,
+  blockedUser: string,
+) => {
   return await prisma.block.create({
     data: {
-      blockedId:blockedUser,
-      blockerId:blockingUser
-    }
-  })
+      blockedId: blockedUser,
+      blockerId: blockingUser,
+    },
+  });
 };
 
-
-export const unblockUserByIDs=async (blockingUser: string,blockedUser: string) => {
+export const unblockUserByIDs = async (
+  blockingUser: string,
+  blockedUser: string,
+) => {
   return await prisma.block.delete({
     where: {
-      blockerId_blockedId:{blockedId:blockedUser,blockerId:blockingUser}
+      blockerId_blockedId: { blockedId: blockedUser, blockerId: blockingUser },
     },
-  })
+  });
 };
 
-
-
-export const getBlockedUsersByID=async (blockingUser: string) => {
-  const user=await prisma.user.findUnique({
-    where:{
-      id:blockingUser
+export const getBlockedUsersByID = async (blockingUser: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: blockingUser,
     },
-    include:{
-      blocker:true
-    }
-  })
-  let blockedUsersIDs=user?.blocker
-  let blockedUsers=[]
-  if(blockedUsersIDs)
-    for(var blockedUserRelations of blockedUsersIDs)
-    {
-      let blockedUser=await prisma.user.findFirst({
-        where:{
-          id:blockedUserRelations.blockedId
-        }
-      })
-      if(blockedUser)
+    include: {
+      blocker: true,
+    },
+  });
+  let blockedUsersIDs = user?.blocker;
+  let blockedUsers = [];
+  if (blockedUsersIDs)
+    for (var blockedUserRelations of blockedUsersIDs) {
+      let blockedUser = await prisma.user.findFirst({
+        where: {
+          id: blockedUserRelations.blockedId,
+        },
+      });
+      if (blockedUser)
         blockedUsers.push({
           userName: blockedUser.userName,
           name: blockedUser.name,
@@ -72,10 +75,8 @@ export const getBlockedUsersByID=async (blockingUser: string) => {
           profileBannerUrl: blockedUser.profileBannerUrl,
           profileImageUrl: blockedUser.profileImageUrl,
           email: blockedUser.email.toLowerCase(),
-        })
+        });
     }
-  
-  return blockedUsers
+
+  return blockedUsers;
 };
-
-
