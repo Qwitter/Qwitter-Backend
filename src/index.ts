33898222c1f048f64b6a7c5ca42bf69dd20f40a4
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-12-03 14:01:46 +0200

    fix: Added cors for sockets

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,14 @@
 import app from './app';
-// // import swaggerDocs from './utils/swagger';
-// import { Server } from 'socket.io';
-// import { createServer } from 'http';
-// import socket from './utils/socket';
-
-// // const port: number = 3000;
-// // app.listen(port, () => {
-// //   swaggerDocs(app, port);
-// //   console.log(`App running on port ${port}...`);
-// // });
-
-// // Socket.io for chatting
-// const httpServer = createServer(app);
-// const io = new Server(httpServer);
-// try {
-//   const socketPort = 5000;
-//   httpServer.listen(socketPort, () => {
-//     console.log(`Socket Running on port ${socketPort}...`);
-//     socket({ io });
-//   });
-// } catch (err) {
-//   console.log(err);
-// }
-import express, { Request, Response } from 'express';
-import bodyParser from 'body-parser';
+import swaggerDocs from './utils/swagger';
 import http from 'http';
+import { Request, Response } from 'express';
 import { Server } from 'socket.io';
+import socket from './utils/socket';
 
 const server = http.createServer(app);
-const io = new Server(server);
-
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+const io = new Server(server, {
+  cors: { origin: '*', credentials: true },
+});
 
 app.get('/messages', (_req: Request, res: Response) => {
   res.send('ffsd');
@@ -50,5 +26,7 @@ io.on('connection', () => {
 
 const PORT = 3000;
 server.listen(PORT, () => {
+  swaggerDocs(app, PORT);
   console.log('server is running on port', PORT);
+  socket({ io });
 });
