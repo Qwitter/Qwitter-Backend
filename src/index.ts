24cd4d890a01b4a14c543ee4bd0a5a9b7ae20947
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-12-03 12:43:00 +0200

    fix: sockets

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,26 @@
 import app from './app';
-import { Server, Socket } from 'socket.io';
-// import swaggerDocs from './utils/swagger';
-// import { Server } from 'socket.io';
-// import { createServer } from 'http';
-// import socket from './utils/socket';
+import swaggerDocs from './utils/swagger';
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import socket from './utils/socket';
 
-// const port: number = 3000;
-// app.listen(port, () => {
-//   swaggerDocs(app, port);
-//   console.log(`App running on port ${port}...`);
-// });
-
-// // Socket.io for chatting
-// const httpServer = createServer();
-// const io = new Server(httpServer, {
-//   cors: { origin: '*', credentials: true },
-// });
-// try {
-//   const socketPort = 5555;
-//   httpServer.listen(socketPort, () => {
-//     console.log(`Socket Running on port ${socketPort}...`);
-//     socket({ io });
-//   });
-// } catch (err) {
-//   console.log(err);
-// }
-// App setup
-const PORT = 5000;
-const server = app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+const port: number = 3000;
+app.listen(port, () => {
+  swaggerDocs(app, port);
+  console.log(`App running on port ${port}...`);
 });
 
-// Static files
-
-// Socket setup
-const io = new Server(server);
-
-io.on('connection', (_socket: Socket) => {
-  console.log('Made socket connection');
+// Socket.io for chatting
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: { origin: '*', credentials: true },
 });
+try {
+  const socketPort = 5000;
+  httpServer.listen(socketPort, () => {
+    console.log(`Socket Running on port ${socketPort}...`);
+    socket({ io });
+  });
+} catch (err) {
+  console.log(err);
+}
