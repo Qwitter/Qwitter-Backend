3714bd35539ed0a6655522f760f882582612f50a
AhmedOsama198 <ahmed.osama1982002@gmail.com>
2023-12-06 19:09:48 +0200

    test: Testing sockets messages

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -17,7 +17,7 @@ const EVENTS = {
 interface CustomSocket extends Socket {
   user: User;
 }
-const rooms: Record<string, { name: string }> = {};
+// const rooms: Record<string, { name: string }> = {};
 
 export function sendRoomMessage(
   socket: CustomSocket,
@@ -33,55 +33,15 @@ export function sendRoomMessage(
 }
 
 function socket({ io }: { io: Server }) {
-  // io.use((_socket, next) => {
-  //   // Extract token from query parameters
-  //   // const token = socket.handshake.query.token;
-
-  //   // if (!token) {
-  //   //   return next(new Error('Authentication error: Token not provided'));
-  //   // }
-
-  //   next();
-
-  //   // jwt.verify(token, secretKey, (err, decoded) => {
-  //   //   if (err) {
-  //   //     return next(new Error('Authentication error: Invalid token'));
-  //   //   }
-
-  //   //   // Attach user information to the socket
-  //   //   socket.user = decoded.user;
-  //   //   next();
-  //   // });
-
-  //   // TODO: Here should be added authentication layer using jwt
-  // });
-
   io.on(EVENTS.connection, (socket: CustomSocket) => {
     console.log('User connected ' + socket.id);
-    const headers = socket.handshake.headers;
-    console.log(headers);
-    socket.emit(EVENTS.SERVER.ROOMS, rooms);
-
-    /*
-     * When a user sends a room message
-     */
-
-    socket.on(EVENTS.CLIENT.SEND_ROOM_MESSAGE, ({ roomId, message }) => {
-      sendRoomMessage(socket, roomId, message);
+    socket.on(EVENTS.CLIENT.SEND_ROOM_MESSAGE, (message) => {
+      console.log('Received room message');
       console.log(message);
     });
-
-    /*
-     * When a user joins a room
-     */
     socket.on(EVENTS.CLIENT.JOIN_ROOM, (roomId) => {
-      //TODO: Check here that the user who wants to join is already in that conversation
-      // if (!socket.user) {
-      //   console.log('unable to join. Please log in first');
-      // }
       console.log('User joined room ' + roomId);
       socket.join(roomId);
-      socket.emit(EVENTS.SERVER.JOINED_ROOM, roomId);
     });
   });
 }
